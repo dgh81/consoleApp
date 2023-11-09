82fa162b84f33ff578cf111c743be9dd2853feb1
DanielGuldberg <danielguldberg@gmail.com>
2023-11-09 08:57:12 +0100

    first commit

---
+++ appSettings.js
@@ -0,0 +1,7 @@
+const settings = {
+    'clientId': 'c92879e8-a662-4f14-9290-f872ad0f8f45',
+    'clientSecret': 'Hy.8Q~2eEhc6rEyK70eQmLUlCMRKXLEtqTSLraD9',
+    'tenantId': '24803583-b63e-427b-857f-ff71b1863978'
+};
+
+module.exports = settings;
